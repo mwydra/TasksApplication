687f6d4cff28402631fa11cb4a39370d57b9eece
mwydra <magdalena.wydra1012@gmail.com>
2018-04-28 19:30:35 +0200

    Moduł 16.4

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,9 +1,12 @@
 package com.crud.tasks;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class TasksApplication {
 
 	public static void main(String[] args) {
