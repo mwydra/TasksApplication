1855ff7859b80ed6c4f25e555ec84ff12161eacb
mwydra <magdalena.wydra1012@gmail.com>
2018-06-18 18:35:57 +0200

    Moduł 24.2

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -1,12 +1,28 @@
 package com.crud.tasks.config;
 
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @Getter
 public class AdminConfig {
 
-    //@Value("${admin.mail}")
-    private String adminMail = "kodilla0course@gmail.com";
+    @Value("${admin.mail}")
+    private String adminMail;
+
+    @Value("${admin.name}")
+    private String adminName;
+
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Value("${info.company.goal}")
+    private String companyGoal;
+
+    @Value("${info.company.email}")
+    private String companyEmail;
+
+    @Value("${info.company.phone}")
+    private String companyPhone;
 }
